254945afefd4114e60ab1d5dc844e09279b1f080
Matt Campbell <campbellhome@gmail.com>
2020-08-27 08:50:54 -0700

    add /exec <path> logic to console command processing

--- src/ui_view_console_command.cpp
+++ src/ui_view_console_command.cpp
@@ -4,6 +4,7 @@
 #include "ui_view_console_command.h"
 #include "bb_array.h"
 #include "bb_string.h"
+#include "file_utils.h"
 #include "fonts.h"
 #include "imgui_core.h"
 #include "imgui_themes.h"
@@ -13,7 +14,9 @@
 #include "recorded_session.h"
 #include "recordings.h"
 #include "sb.h"
+#include "span.h"
 #include "time_utils.h"
+#include "tokenize.h"
 #include "ui_config.h"
 #include "ui_recordings.h"
 #include "va.h"
@@ -68,26 +71,88 @@ static int UIRecordedView_ConsoleInputCallback(ImGuiInputTextCallbackData *Callb
 	return 0;
 }
 
+static void view_console_history_entry_add(view_t *view, const char *command)
+{
+	view->consoleHistory.pos = ~0U;
+	for(u32 i = 0; i < view->consoleHistory.entries.count; ++i) {
+		view_console_history_entry_t *entry = view->consoleHistory.entries.data + i;
+		const char *entryCommand = sb_get(&entry->command);
+		if(!bb_stricmp(entryCommand, command)) {
+			view_console_history_entry_reset(entry);
+			bba_erase(view->consoleHistory.entries, i);
+			break;
+		}
+	}
+	view_console_history_entry_t newEntry = { BB_EMPTY_INITIALIZER };
+	sb_append(&newEntry.command, command);
+	bba_push(view->consoleHistory.entries, newEntry);
+	sb_reset(&view->consoleInput);
+	view->consoleRequestActive = true;
+}
+
+static b32 view_console_send(view_t *view, const char *command)
+{
+	return mq_queue(view->session->outgoingMqId, kBBPacketType_ConsoleCommand, "%s", command);
+}
+
+enum class iniCategory {
+	Unknown,
+	CoreLog,
+	ConsoleVariables,
+};
+
+static void view_console_command_exec(view_t *view, const char *filepath)
+{
+	fileData_t fileData = fileData_read(filepath);
+	if(fileData.bufferSize) {
+		iniCategory category = iniCategory::Unknown;
+		span_t linesCursor = span_from_string((const char *)fileData.buffer);
+		for(span_t line = tokenizeLine(&linesCursor); line.start; line = tokenizeLine(&linesCursor)) {
+			if (line.start == line.end)
+				continue;
+
+			if(span_starts_with(line, "[", kSpanCaseSensitive) && span_ends_with(line, "]", kSpanCaseSensitive)) {
+				if(!span_stricmp(line, span_from_string("[Core.Log]"))) {
+					category = iniCategory::CoreLog;
+				} else if(!span_stricmp(line, span_from_string("[ConsoleVariables]"))) {
+					category = iniCategory::ConsoleVariables;
+				} else {
+					category = iniCategory::Unknown;
+				}
+				continue;
+			}
+
+			if(category == iniCategory::Unknown || span_starts_with(line, ";", kSpanCaseSensitive))
+				continue;
+
+			const char *lineCursor = line.start;
+			span_t key = tokenize(&lineCursor, "= \r\n");
+			span_t val = tokenize(&lineCursor, "= \r\n");
+			if(key.start && val.start) {
+				if(category == iniCategory::CoreLog) {
+					view_console_send(view, va("log %.*s %.*s", (int)(key.end - key.start), key.start, (int)(val.end - val.start), val.start));
+				} else {
+					view_console_send(view, va("%.*s %.*s", (int)(key.end - key.start), key.start, (int)(val.end - val.start), val.start));
+				}
+			}
+		}
+	}
+	fileData_reset(&fileData);
+}
+
 static void UIRecordedView_Console_Dispatch(view_t *view)
 {
 	if(view->session->outgoingMqId != mq_invalid_id() && (view->session->appInfo.packet.appInfo.initFlags & kBBInitFlag_ConsoleCommands) != 0) {
 		const char *command = sb_get(&view->consoleInput);
-		if(mq_queue(view->session->outgoingMqId, kBBPacketType_ConsoleCommand, "%s", command)) {
-			view->consoleHistory.pos = ~0U;
-			for(u32 i = 0; i < view->consoleHistory.entries.count; ++i) {
-				view_console_history_entry_t *entry = view->consoleHistory.entries.data + i;
-				const char *entryCommand = sb_get(&entry->command);
-				if(!bb_stricmp(entryCommand, command)) {
-					view_console_history_entry_reset(entry);
-					bba_erase(view->consoleHistory.entries, i);
-					break;
-				}
+		if(*command == '/') {
+			if(!bb_strnicmp(command, "/exec ", 6)) {
+				view_console_command_exec(view, command + 6);
+				view_console_history_entry_add(view, command);
 			}
-			view_console_history_entry_t newEntry = { BB_EMPTY_INITIALIZER };
-			sb_append(&newEntry.command, command);
-			bba_push(view->consoleHistory.entries, newEntry);
-			sb_reset(&view->consoleInput);
-			view->consoleRequestActive = true;
+		} else {
+			if(view_console_send(view, command)) {
+				view_console_history_entry_add(view, command);
+			}
 		}
 	}
 }
