5da59a75d4bbbd0d8c56e52f064b3008b5ce6438
Matt Campbell <campbellhome@gmail.com>
2022-11-29 16:52:33 -0800

    tweak linux compile commands

--- linux/build_linux.sh
+++ linux/build_linux.sh
@@ -13,7 +13,7 @@ clang++ -g -Werror -Wall -Wextra -I../bbclient/include ../obj/linux/*.o ../bbcli
 
 cd ../obj/linux
 echo Compiling mc_common...
-clang -c -g -Werror -Wall -Wextra -I../../bbclient/include -I../../bbclient/include/bbclient -I../../mc_common/include -I../../thirdparty ../../mc_common/src/*.c ../../thirdparty/parson/parson.c ../../thirdparty/sqlite/sqlite3.c
+clang -c -g -Werror -Wall -Wextra -I../../bbclient/include -I../../bbclient/include/bbclient -I../../mc_common/include -I../../thirdparty ../../mc_common/src/*.c ../../thirdparty/parson/parson.c ../../thirdparty/sqlite/sqlite3.c ../../mc_common/src/uuid_rfc4122/*.c ../../mc_common/src/md5_rfc1321/*.c ../../mc_common/src/mc_callstack/*.c
 
 cd ../../linux
 echo Compiling bboxtolog...
@@ -21,13 +21,5 @@ clang -g -Werror -Wall -Wextra -I../bbclient/include -I../bbclient/include/bbcli
 cp ../bin/linux/bboxtolog ../bin/linux/bbcat
 cp ../bin/linux/bboxtolog ../bin/linux/bbtail
 
-cd ../obj/linux
-echo Compiling mc_common...
-#clang -c -g -Werror -Wall -Wextra -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include/bbclient -I../../submodules/mc_imgui/submodules/mc_common/include -I../../submodules/mc_imgui/submodules/mc_common/submodules ../../submodules/mc_imgui/submodules/mc_common/src/*.c
-#clang -c -g -Werror -Wall -Wextra -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include/bbclient -I../../submodules/mc_imgui/submodules/mc_common/include -I../../submodules/mc_imgui/submodules/mc_common/submodules ../../submodules/mc_imgui/submodules/mc_common/submodules/parson/*.c
-#clang -c -g -Werror -Wall -Wextra -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include/bbclient -I../../submodules/mc_imgui/submodules/mc_common/include -I../../submodules/mc_imgui/submodules/mc_common/submodules ../../submodules/mc_imgui/submodules/mc_common/src/uuid_rfc4122/*.c
-#clang -c -g -Werror -Wall -Wextra -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include/bbclient -I../../submodules/mc_imgui/submodules/mc_common/include -I../../submodules/mc_imgui/submodules/mc_common/submodules ../../submodules/mc_imgui/submodules/mc_common/src/md5_rfc1321/*.c
-#clang -c -g -Werror -Wall -Wextra -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include -I../../submodules/mc_imgui/submodules/mc_common/submodules/bbclient/include/bbclient -I../../submodules/mc_imgui/submodules/mc_common/include -I../../submodules/mc_imgui/submodules/mc_common/submodules ../../submodules/mc_imgui/submodules/mc_common/src/mc_callstack/*.c
-cd ../../linux
 rm -r ../obj/linux/
 echo done
