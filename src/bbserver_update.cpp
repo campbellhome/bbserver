437386d113035c1cc1088814ad063209f6d5be21
Matt Campbell <campbellhome@gmail.com>
2023-05-03 07:36:47 -0700

    add version display if a version is available and updates are disabled

--- src/bbserver_update.cpp
+++ src/bbserver_update.cpp
@@ -434,6 +434,14 @@ void BBServer_MainMenuBar(void)
 			}
 			if (ImGui::BeginMenu("Help"))
 			{
+				if (sb_len(&g_site_config.updates.updateManifestDir) == 0)
+				{
+					const char* currentVersion = Update_GetCurrentVersion();
+					if (currentVersion && *currentVersion)
+					{
+						ImGui::MenuItem(va("version %s", currentVersion), nullptr, false, false);
+					}
+				}
 				if (ImGui::BeginMenu("ImGui"))
 				{
 					ImGui::MenuItem("Demo", nullptr, &s_showImguiDemo);
